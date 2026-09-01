eb0aa53e332fefa34b1d08ed969ccc3b9f6a2b9c
Younes Diouri <younesdiouri@gmail.com>
2026-09-01 19:46:06 +0200

    fix(admin): interdit les mutations de lecture seule
    Refuse explicitement les actions EasyAdmin adressées directement afin que masquer un bouton ne devienne jamais une autorisation implicite.
    
    Refs #260

--- src/Admin/UI/EasyAdmin/ReadOnlyCrudController.php
+++ src/Admin/UI/EasyAdmin/ReadOnlyCrudController.php
@@ -7,7 +7,11 @@ namespace App\Admin\UI\EasyAdmin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /** Les faits métier s'observent ici mais ne se réécrivent jamais dans EasyAdmin. */
 /** @extends AbstractCrudController<object> */
@@ -18,5 +22,20 @@ abstract class ReadOnlyCrudController extends AbstractCrudController
         return $actions
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
             ->disable(Action::NEW, Action::EDIT, Action::DELETE, Action::BATCH_DELETE);
+    }
+
+    public function new(AdminContext $context): KeyValueStore|Response
+    {
+        throw new AccessDeniedHttpException('Cette ressource est en lecture seule.');
+    }
+
+    public function edit(AdminContext $context): KeyValueStore|Response
+    {
+        throw new AccessDeniedHttpException('Cette ressource est en lecture seule.');
+    }
+
+    public function delete(AdminContext $context): KeyValueStore|Response
+    {
+        throw new AccessDeniedHttpException('Cette ressource est en lecture seule.');
     }
 }
